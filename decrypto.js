9d8db11f81d045de6b0735c14e90f76a2c433510
Robin Powell <robin8585@hotmail.com>
2020-05-05 17:58:50 +0100

    Start of proper decrypto.js
    Removed tutorial stuff that was being used for testing and started with some real commands

--- decrypto.js
+++ decrypto.js
@@ -1,39 +1,80 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
+var Logger = require('winston');
+var Auth = require('./Auth.json');
+
+//Global variables
+var GameInProgress = false;
+
+// Configure Logger settings
+Logger.remove(Logger.transports.Console);
+Logger.add(new Logger.transports.Console, {
     colorize: true
 });
-logger.level = 'debug';
+Logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: Auth.token,
    autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    Logger.info('Connected');
+    Logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
+
+        switch(args[0]) {
+            // commands
+            case 'commands':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'All commands must start with !\
+                              \nstart - Starts a new game\
+                              \nend - Ends the current game'
                 });
-            break;
-            // Just add any case commands if you want to..
+                break;
+            case 'start':
+                if (GameInProgress) { 
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'There\'s already a game in progress you big silly'
+                    });
+                } else {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Starting a new game of Decrypto'
+                    });
+
+                    playDecrypto();
+                }
+                break;
+            case 'end':
+                if (!GameInProgress) { 
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'There\'s no game to end, type !start to begin one'
+                    });
+                } else {
+                    endGame();
+
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'You have killed the game, you monster'
+                    });
+                }
+                break;
          }
      }
 });
+
+function playDecrypto() {
+    GameInProgress = true;
+}
+
+function endGame() {
+    GameInProgress = false;
+}
