763a5736127aa8ff0fd01df7d547cf992434311d
Robin Powell <robin8585@hotmail.com>
2020-05-06 18:48:26 +0100

    Now selects a Decryptor, sends them a code, and allows guesses
    Changed some things to match names and conventions in the rulebook.  Added about half the words from the cards in the game box.

--- decrypto.js
+++ decrypto.js
@@ -7,8 +7,12 @@ var Auth = require('./auth.json');
 var AllGames = [];
 
 // Read the clue words from the file
-var Words = Fs.readFileSync("./words.txt").toString();
-var WordList = Words.split(",");
+var Keywords = Fs.readFileSync("./keywords.txt").toString();
+var KeywordList = Keywords.split(",");
+
+// Read the solutions from the file
+var Codes = Fs.readFileSync("./codes.txt").toString();
+var CodeList = Codes.split(",");
 
 // Configure Logger settings
 Logger.remove(Logger.transports.Console);
@@ -63,8 +67,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                               \n!dc rules - Displays the rules\
                               \n!dc start - Starts a new game\
                               \n!dc end - Ends the current game\
-                              \n!dc join purple/green - Joins the purple or green team\
-                              \n!dc ready - Indicates that team selection is complete, and you wish to begin the game'
+                              \n!dc join black/white - Joins the black or white team\
+                              \n!dc ready - Indicates that team selection is complete, and you wish to begin the game\
+                              \n!dc clues clue1, clue2, clue3 - Submit your clues when you are the Encryptor'
                 });
                 break;
             case 'rules':
@@ -88,7 +93,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     bot.sendMessage({
                         to: channelID,
                         message: 'Starting a new game of Decrypto\
-                                  \nJoin a team, using the commands \'!dc join purple\' or \'!dc join green\'\
+                                  \nJoin a team, using the commands \'!dc join black\' or \'!dc join white\'\
                                   \nWhen team selection is complete, type \'!dc ready\' to begin'
                     });
                 }
@@ -106,7 +111,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         message: 'Team selection is not currently in progress'
                     });
                 } else {
-                    if (args[1] == 'purple' || args[1] == 'green') {                   
+                    if (args[1] == 'black' || args[1] == 'white') {                   
                         addPlayerToTeam(userID, user, args[1], gameProperties);
                     } else {
                         bot.sendMessage({
@@ -116,6 +121,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     }
                 }
                 break;
+            // Team selection done, start the game proper
             case 'ready':
                 if (!gameProperties.gameInProgress) {
                     bot.sendMessage({
@@ -128,12 +134,37 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         message: 'There\'s already a game in progress you big silly'
                     });
                 } else {
-                    sendWordsToTeams(gameProperties);
+                    gameProperties.selectingTeams = false;
 
+                    // Short pause between executions helps lessen the info dump that happens here
+                    sendKeywordsToTeams(gameProperties);
+                    setTimeout(() => { sendCodeToNextEncryptor(gameProperties); }, 1000);                      
+                }
+                break;
+            // Clues submitted by the team Encryptor
+            case 'clues':
+                if (!gameProperties.gameInProgress) {
                     bot.sendMessage({
                         to: channelID,
-                        message: 'Your team\'s words have been messaged to you, good luck'
+                        message: 'There\'s no game currently in progress, type \'!dc start\' to begin one'
                     });
+                } else if (gameProperties.selectingTeams) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Team selection is still in progress, chill out with your clues'
+                    });
+                } else if (gameProperties.currentEncryptor.userID != userID) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: gameProperties.currentEncryptor.user + ' is the current Encryptor, I will not be accepting clues from anyone else'
+                    });
+                } else if (gameProperties.currentClues.length == 3) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'You\'ve already submitted your clues, too late to change your mind now'
+                    });
+                } else {
+                    validateAndRecordClues(args, gameProperties);
                 }
                 break;
             // Ends an in progress game
@@ -166,17 +197,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 function addPlayerToTeam(userID, user, team, gameProperties) {
     var player = new Player(userID, user);
     
-    if (team == 'purple') {
-        var playerIndex = gameProperties.purpleTeamMembers.findIndex(element => element.userID == player.userID);
+    if (team == 'black') {
+        var playerIndex = gameProperties.blackTeamMembers.findIndex(element => element.userID == player.userID);
 
         if (playerIndex == -1) {
-            playerIndex = gameProperties.greenTeamMembers.findIndex(element => element.userID == player.userID);
+            playerIndex = gameProperties.whiteTeamMembers.findIndex(element => element.userID == player.userID);
     
             if (playerIndex > -1) {
-                gameProperties.greenTeamMembers.splice(playerIndex, 1);
+                gameProperties.whiteTeamMembers.splice(playerIndex, 1);
             }
 
-            gameProperties.purpleTeamMembers.push(player);
+            gameProperties.blackTeamMembers.push(player);
         } else {
             bot.sendMessage({
                 to: gameProperties.channelID,
@@ -185,16 +216,16 @@ function addPlayerToTeam(userID, user, team, gameProperties) {
             return;
         }
     } else {
-        var playerIndex = gameProperties.greenTeamMembers.findIndex(element => element.userID == player.userID);
+        var playerIndex = gameProperties.whiteTeamMembers.findIndex(element => element.userID == player.userID);
 
         if (playerIndex == -1) {
-            playerIndex = gameProperties.purpleTeamMembers.findIndex(element => element.userID == player.userID);
+            playerIndex = gameProperties.blackTeamMembers.findIndex(element => element.userID == player.userID);
     
             if (playerIndex > -1) {
-                gameProperties.purpleTeamMembers.splice(playerIndex, 1);
+                gameProperties.blackTeamMembers.splice(playerIndex, 1);
             }
 
-            gameProperties.greenTeamMembers.push(player);
+            gameProperties.whiteTeamMembers.push(player);
         } else {
             bot.sendMessage({
                 to: gameProperties.channelID,
@@ -206,60 +237,142 @@ function addPlayerToTeam(userID, user, team, gameProperties) {
 
     Logger.debug('Adding player ' + userID + ' to team ' + team + ' in channel ' + gameProperties.channelID);
 
-    var purpleTeamMembers = [];
-    var greenTeamMembers = [];
+    var blackTeamMembers = [];
+    var whiteTeamMembers = [];
 
-    gameProperties.purpleTeamMembers.forEach(element => purpleTeamMembers.push(element.user));
-    gameProperties.greenTeamMembers.forEach(element => greenTeamMembers.push(element.user));
+    gameProperties.blackTeamMembers.forEach(element => blackTeamMembers.push(element.user));
+    gameProperties.whiteTeamMembers.forEach(element => whiteTeamMembers.push(element.user));
 
     bot.sendMessage({
         to: gameProperties.channelID,
         message: user + ' has joined the ' + team + ' team\
-                 \nPurple Team - ' + purpleTeamMembers + '\
-                 \nGreen Team - ' + greenTeamMembers
+                 \nBlack Team - ' + blackTeamMembers + '\
+                 \nWhite Team - ' + whiteTeamMembers
     });
 }
 
-function sendWordsToTeams (gameProperties) {    
-    var wordsToUse = [];
-    while(wordsToUse.length < 8) {
-        var word = WordList[Math.floor(Math.random()*WordList.length)];
-        if (wordsToUse.indexOf(word) == -1) {
-            wordsToUse.push(word);
+// Randomly selects 4 keywords for each team and sends them to team members
+function sendKeywordsToTeams (gameProperties) {    
+    var keywordsToUse = [];
+    while(keywordsToUse.length < 8) {
+        var keyword = KeywordList[Math.floor(Math.random()*KeywordList.length)];
+        if (keywordsToUse.indexOf(keyword) == -1) {
+            keywordsToUse.push(keyword);
         }
     }
 
-    gameProperties.purpleTeamMembers.forEach(element => {
+    gameProperties.blackTeamMembers.forEach(element => {
         bot.sendMessage({
             to: element.userID,
-            message: 'Your team\'s words are:\
-                      \n1 - ' + wordsToUse[0] + '\
-                      \n2 - ' + wordsToUse[1] + '\
-                      \n3 - ' + wordsToUse[2] + '\
-                      \n4 - ' + wordsToUse[3]
+            message: 'Your team\'s keywords are:\
+                      \n1 - ' + keywordsToUse[0] + '\
+                      \n2 - ' + keywordsToUse[1] + '\
+                      \n3 - ' + keywordsToUse[2] + '\
+                      \n4 - ' + keywordsToUse[3]
         });
     })
 
-    gameProperties.greenTeamMembers.forEach(element => {
+    gameProperties.whiteTeamMembers.forEach(element => {
         bot.sendMessage({
             to: element.userID,
-            message: 'Your team\'s words are:\
-                      \n1 - ' + wordsToUse[4] + '\
-                      \n2 - ' + wordsToUse[5] + '\
-                      \n3 - ' + wordsToUse[6] + '\
-                      \n4 - ' + wordsToUse[7]
+            message: 'Your team\'s keywords are:\
+                      \n1 - ' + keywordsToUse[4] + '\
+                      \n2 - ' + keywordsToUse[5] + '\
+                      \n3 - ' + keywordsToUse[6] + '\
+                      \n4 - ' + keywordsToUse[7]
         });
     })
+
+    bot.sendMessage({
+        to: gameProperties.channelID,
+        message: 'Your team\'s keywords have been messaged to you, good luck\
+                  \n---------------------------------------------------------------------------'
+    });
 }
 
-//Ends the game by resetting its properties
+// Randomly select a code and send it to the next encryptor
+function sendCodeToNextEncryptor (gameProperties) {
+    gameProperties.currentCode = CodeList[Math.floor(Math.random()*CodeList.length)];
+    gameProperties.currentTeam = gameProperties.nextTurn;
+    
+    if (gameProperties.currentTeam == 'black') {
+        gameProperties.currentEncryptor = gameProperties.blackTeamMembers[gameProperties.nextBlackEncryptor];
+
+        gameProperties.nextTurn = 'white';
+        gameProperties.nextBlackEncryptor += 1;
+
+        if (gameProperties.nextBlackEncryptor == gameProperties.blackTeamMembers.length) {
+            gameProperties.nextBlackEncryptor = 0;
+        }
+    } else {
+        gameProperties.currentEncryptor = gameProperties.whiteTeamMembers[gameProperties.nextWhiteEncryptor];
+
+        gameProperties.nextTurn = 'black';
+        gameProperties.nextWhiteEncryptor += 1;
+
+        if (gameProperties.nextWhiteEncryptor == gameProperties.whiteTeamMembers.length) {
+            gameProperties.nextWhiteEncryptor = 0;
+        }
+    }
+
+    bot.sendMessage({
+        to: gameProperties.currentEncryptor.userID,
+        message: 'Your code is ' + gameProperties.currentCode
+    });
+    bot.sendMessage({
+        to: gameProperties.channelID,
+        message: 'It is the ' + gameProperties.currentTeam + ' team\'s turn\
+                 \n' + gameProperties.currentEncryptor.user + ' is their Encryptor this round, and has been sent a code\
+                 \nWhen you are ready ' + gameProperties.currentEncryptor.user + ', submit your clues using \'!dc clues clue1, clue2, clue3\'\
+                 \nDon\'t forget the commas, I won\'t understand you otherwise'
+    });
+}
+
+// Validate that 3 clues have been submitted and record them
+function validateAndRecordClues(args, gameProperties) {    
+    var cluesList = "";
+    
+    // args is split by ' ', need to recombine and split by ', ' to get the clues
+    // args[0] will always have been the keyword 'clues', so start at 1
+    for (var i = 1; i < args.length; i++) {
+        cluesList = cluesList + " " + args[i];
+    }
+
+    gameProperties.currentClues = cluesList.split(',');
+
+    if (gameProperties.currentClues.length == 3) {
+        bot.sendMessage({
+            to: gameProperties.channelID,
+            message: 'The clues for this round are:\
+                      \n' + gameProperties.currentClues[0] + '\
+                      \n' + gameProperties.currentClues[1] + '\
+                      \n' + gameProperties.currentClues[2]
+        });
+    } else {
+        gameProperties.currentClues = [];
+
+        bot.sendMessage({
+            to: gameProperties.channelID,
+            message: 'You have to submit 3 clues, you have submitted ' + gameProperties.currentClues.length
+        });
+    }
+}
+
+// Ends the game by resetting its properties
 function endGame(gameProperties) {
     Logger.debug('Ending game in channel ' + gameProperties.channelID);
 
     gameProperties.gameInProgress = false;
     gameProperties.selectingTeams = false;
-    gameProperties.purpleTeamMembers = [];
-    gameProperties.greenTeamMembers = [];
+    gameProperties.blackTeamMembers = [];
+    gameProperties.nextBlackEncryptor = 0;
+    gameProperties.whiteTeamMembers = [];
+    gameProperties.nextWhiteEncryptor = 0
+    gameProperties.nextTurn = 'white';
+    gameProperties.currentCode = 0;
+    gameProperties.currentTeam = ""
+    gameProperties.currentEncryptor = null;
+    gameProperties.currentClues = []
 }
 
 // Each game in progress is stored in an instance of GameProperties
@@ -267,10 +380,17 @@ function GameProperties (channelID, now) {
     this.channelID = channelID;
     this.gameInProgress = false;
     this.selectingTeams = false;
-    this.purpleTeamMembers = [];
-    this.greenTeamMembers = [];
+    this.blackTeamMembers = [];
+    this.nextBlackEncryptor = 0
+    this.whiteTeamMembers = [];
+    this.nextWhiteEncryptor = 0
     this.lastGameStartTime = now;
-};
+    this.nextTurn = 'white';
+    this.currentCode = 0;
+    this.currentTeam = "";
+    this.currentEncryptor = null;
+    this.currentClues = [];
+}
 
 // Each player in a team is stored in an instance of Player
 function Player (userID, user) {
